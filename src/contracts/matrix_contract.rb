8fbc7d2dfb7c29156e2554910fe83967c1540a2e
isupeene <isupeene@ualberta.ca>
2014-09-19 12:53:10 -0600

    Added the initial matrix contracts file with a sample pre- and postcondition.

---
+++ src/contracts/matrix_contract.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+module MatrixTest 
+        include Test::Unit::Assertions
+
+        ##############
+        # Properties #
+        ##############
+        
+        def diagonal_precondition
+        end
+
+        def diagonal_postcondition(result)
+                assert_equal(each_with_index.all?{ |x, i, j| i == j || x == 0 }, result)
+        end
+end
