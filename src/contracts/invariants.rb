5c735c441b2665298482918a479c12574b6f7e4f
Isaac Supeene <isupeene@ualberta.ca>
2014-09-20 19:13:50 -0600

    Added informative error messages for the contract assertion failures.

--- src/contracts/invariants.rb
+++ src/contracts/invariants.rb
@@ -6,7 +6,12 @@ module Invariants
 	def const(*args)
 		temp = clone
 		yield
-		assert_equal(temp, self)
+		assert_equal(
+			temp, self,
+			"The object was altered by a const method.\n" \
+			"Before: #{temp}\n" \
+			"After: #{self}"
+		)
 	end
 end
 
--- src/contracts/matrix_contract.rb
+++ src/contracts/matrix_contract.rb
@@ -15,13 +15,20 @@ module MatrixContract
         ##############
         
         def diagonal_precondition?
-		assert square?
+		assert(
+			square?,
+			"#diagonal? can only be called on a square matrix.\n" \
+			"This matrix is #{row_size} by #{column_size}"
+		)
         end
 
         def diagonal_postcondition?(result)
                 assert_equal(
 			each_with_index.all?{ |x, i, j| i == j || x == 0 },
-			result
+			result,
+			"#diagonal? returned an incorrect result.\n" \
+			"Returned #{result} for the following matrix:\n" \
+			"#{self}"
 		)
         end
 
