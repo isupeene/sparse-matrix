5285c30a5a0722bac7519e39734e5af4ca66050d
Isaac Supeene <isupeene@ualberta.ca>
2014-09-21 12:46:17 -0600

    Added an improved prototype for const methods.

--- prototype/contracts_test.rb
+++ prototype/contracts_test.rb
@@ -1,5 +1,19 @@
 require 'test/unit'
 
+class Module
+	def curry(new_method, original_method, *pre_args)
+		define_method(new_method) do |*args|
+			send(original_method, *pre_args, *args)
+		end
+	end
+
+	def block_curry(new_method, original_method, *pre_args)
+		define_method(new_method) do |*args, &block|
+			send(original_method, *pre_args, *args, &block)
+		end
+	end
+end
+
 module Contract
 	def included(type)
 		require_method_invariants(type)
@@ -132,16 +146,50 @@ end
 
 module Invariants
 	include Test::Unit::Assertions
-	def const(*args)
-		temp = clone
-		yield
-		assert_equal(temp, self)
+
+	def const(method_name)
+		add_invariant_contract(method_name) do |instance, *args, &block|
+			temp = instance.clone
+			puts "const before"
+			block.call
+			assert_equal(
+				temp, instance,
+				"const method #{instance.class.name}.#{method_name} " \
+				"modified instance.\n"
+			)
+			puts "const after"
+		end
+	end
+
+	def add_invariant_contract(method_name, &block)
+		if ['?', '!', '='] === method_name[-1]
+			root_name = method_name[0...-1]
+			suffix = method_name[-1]
+		else
+			root_name = method_name.to_s
+			suffix = ""
+		end
+		invariant_method_name = root_name + "_invariant" + suffix
+
+		if method_defined?(invariant_method_name)
+			original_method = instance_method(invariant_method_name)
+			define_method(invariant_method_name) do |*args, &b|
+				block.call(self, *args) {
+					original_method.bind(self).call(*args, &b)
+				}
+			end
+		else
+			define_method(invariant_method_name) do |*args, &b|
+				block.call(self, *args, &b)
+			end
+		end
 	end
 end
 
 module SquarerContract
-	include Invariants
+	extend Invariants
 	extend Contract
+	include Test::Unit::Assertions
 
 	def square_precondition(x)
 		puts self
@@ -159,9 +207,6 @@ module SquarerContract
 
 	def square_root_postcondition(x, result)
 		assert_equal(Math::sqrt(x), result)
-	end
-
-	def square_precondition?(x, y)
 	end
 
 	def square_postcondition?(x, y, result)
@@ -186,7 +231,9 @@ module SquarerContract
 		puts "hello from class invariant"
 	end
 
-	alias square_root_invariant const
+	const :square_root
+	const :square_root
+	const :square?
 end
 
 class Squarer
@@ -209,7 +256,8 @@ class Squarer
 	end
 
 	def ==(other)
-		return other.is_a?(Squarer)
+		#return false
+		#return other.is_a?(Squarer)
 	end
 
 	include SquarerContract
