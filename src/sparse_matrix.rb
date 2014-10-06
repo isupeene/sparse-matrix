7615504e7128ddfcf3e127ed3f3c23c70584d4e4
bsoetaer <bsoetaer@ualberta.ca>
2014-10-05 18:24:31 -0700

    Constructor that actually works. Long param list for initialize though

--- src/sparse_matrix.rb
+++ src/sparse_matrix.rb
@@ -1,40 +1,43 @@
-require "contracts/MatrixContract"
+require_relative "contracts/matrix_contract"
 
-Class SparseMatrix
+class SparseMatrix
 	
-	def initialize(row_size, column_size)
+	def initialize(row_size, column_size, nonzero_values, col_ind, row_ptr)
 		@row_size = row_size
 		@column_size = column_size
-		@nonzero_values = []
-		@col_ind = []
-		@row_ptr = [0]
+		@nonzero_values = nonzero_values
+		@col_ind = col_ind
+		@row_ptr = row_ptr
 	end
 	
-	def build(row_size, column_size)
-		initialize(row_size, column_size)
+	def self.build(row_size, column_size)
+		nonzero_values, col_ind, row_ptr = [], [], [0]
 		row_size.times { |i| 
 			row_count = 0
 			column_size.times { |j| 
 				value = yield i, j
+				puts value
 				unless value == 0
-					@nonzero_values.push(value)
-					@col_ind.push(j)
+					nonzero_values.push(value)
+					col_ind.push(j)
 					row_count += 1
 				end
 			}
-			@row_ptr.push(@row_ptr.last + row_count)
+			row_ptr.push(row_ptr.last + row_count)
 		}
+		new row_size, column_size, nonzero_values, col_ind, row_ptr
 	end
 	
-	def build_from_hash(row_size, column_size, h)
-		initialize(row_size, column_size)
+	def self.build_from_hash(row_size, column_size, h)
+		nonzero_values, col_ind, row_ptr = [], [], [0]
 		h.keys.sort.each { |i|
 			h[i].keys.sort.each { |j|
-				@nonzero_values.push(h[i][j])
-				@col_ind.push(j)
+				nonzero_values.push(h[i][j])
+				col_ind.push(j)
 			}
-			@row_ptr.push(@row_ptr.last + a[i].keys.count)
+			row_ptr.push(row_ptr.last + a[i].keys.count)
 		}
+		new row_size, column_size, nonzero_values, col_ind, row_ptr
 	end
 	
 end
