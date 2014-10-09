28d128c249d5a496d1831249bd3ccd26be18566e
bsoetaer <bsoetaer@ualberta.ca>
2014-10-09 11:17:49 -0700

    Added Contract Decorator to Builders.

--- src/builders/complete_matrix_builder.rb
+++ src/builders/complete_matrix_builder.rb
@@ -1,22 +1,25 @@
-require_relative 'matrix_builder'
-require_relative 'complete_vector_builder'
 require_relative '../matrix'
+require_relative 'matrix_builder'
+require_relative '../contracts/contract_decorator'
 require_relative '../contracts/matrix_builder_contract'
+require_relative 'implementations/complete_matrix_builder_impl'
 
-class CompleteMatrixBuilder < MatrixBuilder
+class CompleteMatrixBuilder
+	include ContractDecorator
+	include MatrixBuilderContract
+
+	def initialize(*args, &block)
+		#super.register :complete
+		super(CompleteMatrixBuilderImpl.send(:new, *args, &block))
+	end
+	
+	def builder_type
+		:complete
+	end
+	
 	def self.method_missing(symbol, *args, &block)
 		Matrix.public_send(symbol, *args, &block)
 	end
-
-	def to_mat
-		Matrix.build(row_size, column_size){ |i, j| self[i, j] }
-	end
-
-	def builder_type
-		:complete # Doesn't really matter, as we don't use the vectors anyway
-	end
-
-	register :complete
 
 	include MatrixBuilderContract
 end
